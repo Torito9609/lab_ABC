7b168c972cb321bf04ba0f437e31eab927e15001
Torito9609 <toroandres85@gmail.com>
2024-12-19 14:30:47 -0500

    se implementa la funcion cambio color tags h5

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function random_Color() {
-  const colors = ["yellow", "blue", "red"];
+  const colors = ["green", "blue", "red"];
   random_index = Math.floor(Math.random() * colors.length);
   return colors[random_index];
+}
+
+function change_Color(element) {
+  const random_color = random_Color();
+  element.style.color = random_color;
 }
